3e125968bada862191eb76694c3e484bbb0f2119
yvettecook <yvettecook@gmail.com>
2014-11-07 15:23:06 +0000

    User can log in from the homepage

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'On the homepage' do
 
-  context 'a new user wanting to sign up' do
+  context 'a new user' do
 
     it 'can click on the sign up button' do
       visit '/'
@@ -10,6 +10,23 @@ describe 'On the homepage' do
       expect(page).to have_content 'Email'
       expect(page).to have_content 'Password'
       expect(page).to have_content 'Password confirmation'
+    end
+
+  end
+
+  context 'a registered user' do
+
+    before do
+      User.create(email: 'yvette@test.com', password: 'testtest', password_confirmation: 'testtest')
+    end
+
+    it 'can log in' do
+      visit '/'
+      click_link 'Sign In'
+      fill_in 'Email', with: 'yvette@test.com'
+      fill_in 'Password', with: 'testtest'
+      click_button 'Log in'
+      expect(page).to have_content 'Welcome yvette@test.com'
     end
 
   end
