45b05a7fecab905fc75500b554edb8a740acee11
yvettecook <yvettecook@gmail.com>
2014-11-07 18:55:44 +0000

    Posts are belong to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107170703) do
+ActiveRecord::Schema.define(version: 20141107185121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,10 @@ ActiveRecord::Schema.define(version: 20141107170703) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,1 +1,15 @@
+require 'rails_helper'
 
+describe 'Posts' do
+
+  context 'are associated with' do
+
+    it 'a user' do
+      user = User.create(email: 'yvette@test.com', password: 'testtest', password_confirmation: 'testtest')
+      post = Post.create(description: 'sunset', user_id: user.id)
+      expect(post.user.email).to eq 'yvette@test.com'
+    end
+
+  end
+
+end
